83ca104df5148355dad0fc4521827f16f246fcca
Almir Marques <almir.marques@valtech.com>
2023-01-13 12:59:47 -0300

    Development

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,24 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    static: './dist',
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Development',
     }),
   ],
   module: {
@@ -31,5 +37,8 @@ module.exports = {
         type: 'asset/resource',
       },
     ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
